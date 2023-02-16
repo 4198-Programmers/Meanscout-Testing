8f638a0d3e4a4de92b2ea115e77c79039dfd6b3f
Aiden <aidenjlee9478@gmail.com>
2023-02-16 09:18:45 -0600

    Base version
    Has Ability to change link it sends to, and password, and how many times it sends

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use clap::{Args, Parser, Subcommand};
+mod requests;
+
+/// Program to test the Meanscout API
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+   /// Number of times to do
+   #[arg(short, long, default_value_t = 1)]
+   count: u8,
+
+   /// Change the link if needed
+   #[arg(short, long, default_value="https://data.team4198.org:8000/scouting")]
+   link: String,
+
+   /// Password to be used with API
+   #[arg(short, long, default_value="ChangeMe!")]
+   password: String,
+
+//    #[command(subcommand)]
+//     command: Option<Commands>,
+
+}
+
+// #[derive(Subcommand, Debug)]
+// enum Commands {
+//     /// Adds
+//     Add(Add),
+//     /// does a thing
+//     Remove(Add),
+// }
+
+// #[derive(Args, Debug)]
+// struct Add {
+//     name: Option<String>,
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    for _i in 0..args.count {
+        let request = requests::post_data(&args.link, &args.password);
+        if request.is_ok() {
+            println!("it worked");
+        }
+        else {
+            println!("Something went wrong");
+            break
+        }
+    }
+    
+    // This is just some testing with commands
+    // if args.command.is_some() {
+    //     match &args.command.as_ref().unwrap() {
+    //         Commands::Add(name) => {
+    //             println!("'meantest add' was used, name is: {:?}", name.name.as_ref().unwrap())
+    //         },
+    //         Commands::Remove(name) => {
+    //             println!("'meantest remove' was used, name is: {:?}", name.name.as_ref().unwrap())
+    //         }
+    //     }
+    // }
+
+//    for _ in 0..args.count {
+//        println!("{}", args.password);
+        // println!("{:?}", &args.command.is_some());
+//    }
 }
---
+++ src/requests.rs
@@ -0,0 +1,109 @@
+use reqwest;
+use rand::{self, Rng};
+use serde::{Serialize, Deserialize};
+
+pub fn post_data(link: &str, password: &str) -> Result<(), reqwest::Error> {
+    let data = TempData {
+        team: random_number(1, 10000).to_string(),
+        matchnum: random_number(1, 100).to_string(),
+        absent: false,
+        name: "Test".to_string(),
+        location: "Red 1".to_string(),
+        teamleftcommu: random_bool(),
+        teamcollected: random_bool(),
+        autochargesta: random_bool(),
+        topcubes: random_number(0, 10),
+        middlecubes: random_number(0, 10),
+        bottomcubes: random_number(0, 10),
+        missedcubes: random_number(0, 10),
+        topcones: random_number(0, 10),
+        middlecones: random_number(0, 10),
+        bottomcones: random_number(0, 10),
+        missedcones: random_number(0, 10),
+
+        topcube: random_number(0, 10),
+        middlecube: random_number(0, 10),
+        bottomcube: random_number(0, 10),
+        missedcube: random_number(0, 10),
+        topcone: random_number(0, 10),
+        middlecone: random_number(0, 10),
+        bottomcone: random_number(0, 10),
+        missedcone: random_number(0, 10),
+        defenceplayti: random_float(0.0, 20.0),
+        defensiverati: random_number(0, 6),
+        teamattemptsc: random_bool(),
+        chargestation: "Who knows".into(),
+        links: random_number(0, 7),
+        anyrobotprobl: "Absolutely".into(),
+        fouls: "Not much".into(),
+        extranotes: "White Woman Jumpscare!".into(),
+        driveteamrati: "They sure drove".into(),
+        playstylesumm: "Something".into(),
+        password: password.into(),
+    };
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(link).json(&data).send()?;
+    // println!("{}", res.as_ref().);
+    println!("{}", res.status());
+    Ok(())
+
+}
+
+pub fn random_number(lower: i64, upper: i64) -> i64 {
+    let num = rand::thread_rng().gen_range(lower..upper);
+    return num
+}
+
+pub fn random_float(lower: f64, upper: f64) -> f64 {
+    let num = rand::thread_rng().gen_range(lower..upper);
+    return num
+}
+pub fn random_bool() -> bool {
+    let thing = rand::thread_rng().gen_range(0..100);
+    if 0 == thing%2 {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TempData {
+    pub team: String,
+    pub matchnum: String,
+    pub absent: bool,
+    pub name: String,
+    pub location: String,
+    pub teamleftcommu: bool,
+    pub teamcollected: bool,
+    pub autochargesta: bool,
+    pub topcubes: i64,
+    pub middlecubes: i64,
+    pub bottomcubes: i64,
+    pub missedcubes: i64,
+    pub topcones: i64,
+    pub middlecones: i64,
+    pub bottomcones: i64,
+    pub missedcones: i64,
+    pub topcube: i64,
+    pub middlecube: i64,
+    pub bottomcube: i64,
+    pub missedcube: i64,
+    pub topcone: i64,
+    pub middlecone: i64,
+    pub bottomcone: i64,
+    pub missedcone: i64,
+    pub defenceplayti: f64,
+    pub defensiverati: i64,
+    pub teamattemptsc: bool,
+    pub chargestation: String,
+    pub links: i64,
+    pub anyrobotprobl: String,
+    pub fouls: String,
+    pub extranotes: String,
+    pub driveteamrati: String,
+    pub playstylesumm: String,
+    pub password: String,
+}
